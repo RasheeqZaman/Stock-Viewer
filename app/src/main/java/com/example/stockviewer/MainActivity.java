627de8da7de0e55d4f63cedb26fdae3a21aee82e
Rasheeq Zaman <zamanrshq@gmail.com>
2020-09-29 21:48:50 +0600

    Added Toolbar.

--- app/src/main/java/com/example/stockviewer/MainActivity.java
+++ app/src/main/java/com/example/stockviewer/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.stockviewer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        MenuItem search = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) search.getActionView();
+        return super.onCreateOptionsMenu(menu);
     }
 }
